6aeb3fcb7529e58384fc25aa7f19b483f581c1b4
ofts-cqm <qianmuchen.sam@gmail.com>
2026-07-19 09:57:56 -0400

    feat: initial commit

---
+++ Assets/Scripts/render/UCChunkMesh.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace render;
+
+public class UCChunkMesh : MonoBehaviour
+{
+    public MeshFilter MeshFilter;
+    public MeshRenderer MeshRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
